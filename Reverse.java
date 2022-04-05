9cb5baf909674452cd48776582f0fbe445a48491
vijaychander321 <102986912+vijaychander321@users.noreply.github.com>
2022-04-05 11:38:18 +0530

    Add files via upload
    reverse of a number

---
+++ Reverse.java
@@ -0,0 +1,21 @@
+package reverse;
+
+import java.util.Scanner;
+
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int org=n;
+        int rev=0,rem;
+        while(n!=0) {
+        	rem=n%10;
+        	rev=rev*10+rem;
+        	n=n/10;
+        }
+        System.out.print(org+" reverse number: "+rev);
+	}
+
+}
